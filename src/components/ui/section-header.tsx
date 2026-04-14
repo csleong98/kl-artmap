e775aba4705e51c125e0123aac629783bdafdd58
csleong98 <macintoshleong@gmail.com>
2026-04-14 22:15:22 +0800

    added flower patterns to the section header

--- src/components/ui/section-header.tsx
+++ src/components/ui/section-header.tsx
@@ -9,15 +9,21 @@ export default function SectionHeader({ title }: SectionHeaderProps) {
         {title}
       </p>
       <div className="flex gap-[4px] items-center">
-        <div className="w-[15px] h-[15px] flex items-center justify-center">
-          <div className="w-[4px] h-[4px] bg-[#d4d4d4] rounded-full" />
-        </div>
-        <div className="w-[15px] h-[15px] flex items-center justify-center">
-          <div className="w-[4px] h-[4px] bg-[#d4d4d4] rounded-full" />
-        </div>
-        <div className="w-[15px] h-[15px] flex items-center justify-center">
-          <div className="w-[4px] h-[4px] bg-[#d4d4d4] rounded-full" />
-        </div>
+        <img
+          src="/assets/flower pattern.svg"
+          alt=""
+          className="w-[15px] h-[15px]"
+        />
+        <img
+          src="/assets/flower pattern.svg"
+          alt=""
+          className="w-[15px] h-[15px]"
+        />
+        <img
+          src="/assets/flower pattern.svg"
+          alt=""
+          className="w-[15px] h-[15px]"
+        />
       </div>
     </div>
   );
